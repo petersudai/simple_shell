20c0fb4e2f3e1ccae4ba46361ac18fd9c60e8104
petersudai <psudai@gmail.com>
2023-11-14 22:45:16 -0800

    updating main.c

--- main.c
+++ main.c
@@ -2,10 +2,30 @@
 
 /**
  * main - Entry point of the program
+ * @argc: the number of arguments
+ * @argv: the array of arguments
+ *
  * Return: 0 on success
  */
-int main(void)
+int main(int argc, char *argv[])
 {
-	hsh();
+	int i, j;
+	char **tokens;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			tokens = parse_command(argv[i]);
+			execute_command(tokens);
+
+			for (i = 0; tokens[j] != NULL; j++)
+				free(tokens[j]);
+			free(tokens);
+		}
+	}
+	else
+		hsh();
+
 	return (0);
 }
